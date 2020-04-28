02c410e7498751d27f347dd50c4992deb5e0c866
Panarat D <panarat.duke@gmail.com>
2020-04-28 15:43:40 -0500

    add scrape save load and get

--- application.py
+++ application.py
@@ -6,26 +6,28 @@ import mars_scraping
 app = Flask(__name__)
 
 # Set up Mongo
-mongo_conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(mongo_conn)
-db=client.mars_db
+# mongo_conn = 'mongodb://localhost:27017'
+# client = pymongo.MongoClient(mongo_conn)
+# db=client.mars_db
 
 # Set route
 @app.route('/')
 def index():
     try:
-        news = mars_scraping.get_results()
+        news = mars_scraping.get_mars_info()
     except:
         scrape()
-        news = mars_scraping.get_results()
+        news = mars_scraping.get_mars_info()
     return render_template(
         'index.html', 
         mars_news=news)
 
 @app.route('/scrape')
 def scrape():
-    mars_scraping.scrape_news():
+    # mars_scraping.scrape_news():
     mars_scraping.save_latest_news():
+    mars_scraping.scrape_weather():
+    mars_scraping.save_weather():
 
     
 
--- mars_scraping.py
+++ mars_scraping.py
@@ -4,6 +4,7 @@ import os
 from splinter import Browser
 import time
 import pymongo
+import pandas as pd
 
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
@@ -108,19 +109,77 @@ def save_weather():
     db.weather.insert_one(scrape_weather())
     print("Success load into database")
 
+def scrape_facts():
+    # Return Var
+    facts_dict = {}
 
-def get_latest_news():
+    #---------------- Scrape Facts from Space-Facts --------------------#
+    url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url)
+    target_table=tables[0]
+
+    for each_row in target_table.index:
+        each_row_df=target_table.loc[each_row]
+        facts_dict[each_row_df[0]]=each_row_df[1]
+
+    return facts_dict
+
+def save_facts():
     conn = 'mongodb://localhost:27017'
-    # Pass connection to the pymongo instance.
     client = pymongo.MongoClient(conn)
-    # Connect to a database. Will create one if not already available.
     db = client.mars_db
-    mars_latest_news = db.latest_news.find()
-    return mars_latest_news
+    db.facts.drop()
+    print("\nAttempting to load data...")
+    db.facts.insert_one(scrape_facts())
+    print("Success load into database")
 
+def scrape_hemispheres():
+    # Variables
+    img_url = ""
+    titles = ""
+    # Return Var
+    hemisphere_image_urls ={}
 
-
+    #---------------- Scrape Facts from Space-Facts --------------------#
+    usgs_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(usgs_url)
+    html = browser.html
     
+    soup = BeautifulSoup(html, 'html.parser')
+    titles = soup.find_all('div', class_="item")
+
+    for each_title in titles: 
+        key_word = each_title.find('h3').text
+        print(key_word)
+        browser.find_link_by_partial_text(key_word).click()
+        img_url = browser.find_link_by_text('Sample').first['href']
+        print(img_url)
+        hemisphere_image_urls[key_word]=img_url
+        browser.back()
+
+    return hemisphere_image_urls
+    
+def save_hemispheres():
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+    db = client.mars_db
+    db.hemispheres.drop()
+    print("\nAttempting to load data...")
+    db.facts.insert_one(scrape_hemispheres())
+    print("Success load into database")
+
+def get_mars_info():
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+    db = client.mars_db
+    latest_news = db.latest_news.find()
+    feature_img = db.mars_space_img.find()
+    weather = db.weather.find()
+    facts = db.facts.find()
+    hemisphere = db.hemisphere.find()
+
+    return (latest_news,feature_img,weather,facts,hemisphere)
+
 
 
 
