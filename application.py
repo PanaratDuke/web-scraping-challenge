1c564fdcbd29e88b363e1cc647f29273441a55fd
Panarat D <panarat.duke@gmail.com>
2020-04-28 15:56:32 -0500

    update scrape save load get

--- application.py
+++ application.py
@@ -14,22 +14,29 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     try:
-        news = mars_scraping.get_mars_info()
+        news, fimg, weat, fact, hemis = mars_scraping.get_mars_info()
     except:
         scrape()
-        news = mars_scraping.get_mars_info()
+        news, fimg, weat, fact, hemis = mars_scraping.get_mars_info()
     return render_template(
         'index.html', 
-        mars_news=news)
+        m_news=news,
+        m_fimg=fimg,
+        m_weat=weat,
+        m_fact=fact,
+        m_hemis=hemis
+        )
 
 @app.route('/scrape')
 def scrape():
-    # mars_scraping.scrape_news():
+    mars_scraping.scrape_news():
     mars_scraping.save_latest_news():
     mars_scraping.scrape_weather():
     mars_scraping.save_weather():
-
-    
+    mars_scraping.scrape_facts()
+    mars_scraping.save_facts()
+    mars_scraping.scrape_hemispheres()
+    mars_scraping.save_hemispheres()  
 
 if __name__ == "__main__":
     app.run(debug=True)
